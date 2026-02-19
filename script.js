03d4dc000a1587c3813794d538c3ef24008cddec
Cameron Cruz <134180102+Cameron1Cruz@users.noreply.github.com>
2026-02-18 22:00:13 -0600

    Add hover drawing effect to grid squares.

---
+++ script.js
@@ -0,0 +1,22 @@
+const container = document.querySelector("#container");
+
+function createGrid(size) {
+    container.innerHTML = "";
+    const totalSquares = size * size;
+    const squareSize = 960 / size;
+
+    for (let i = 0; i < totalSquares; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+
+        square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = "black";
+    });
+    
+        container.appendChild(square);
+    }
+}
+
+createGrid(16);
